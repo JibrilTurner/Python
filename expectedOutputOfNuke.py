67d330fd9262bc3579ce83b120641f8d2f14d46f
JibrilTurner <121262046+JibrilTurner@users.noreply.github.com>
2023-01-25 10:13:22 -0600

    Update expectedOutputOfNuke.py

--- expectedOutputOfNuke.py
+++ expectedOutputOfNuke.py
@@ -1,3 +1,6 @@
+# 1/24/23 Made By Jibril 
+# https://en.wikipedia.org/wiki/Intercontinental_ballistic_missile#:~:text=Flight%20phases,-The%20following%20flight&text=reentry%2Fterminal%20phase%20(starting%20at,see%20also%20maneuverable%20reentry%20vehicle. 
+
 # General Info 
 # V = Velocity
 # Δ = Change in X 
@@ -7,42 +10,91 @@
 # Projectile Info 
 # Name = X 
 # Total Moves = X
-# Radis = X 
+# Explotion Radis = X 
+# totalMoves = 5
 
 # Function 
 # 
 # 
 #
 # Notes 
-# 
+# You will Start with Three and are not Renewable 
 #
+# It Will Dednate at Y30Km, Rather than Y0Km. Because a
+# of a AirBurst  
 #
+# I might add a alt deadnation in the future where it denaates 
+# at Y0Km to add variton in use
 
-# Velocity Range = 0Kmh -> Kmh  -> 0Mph -> Mph
-# X-Axis Range   = 0km  -> Km   -> 0Mi  -> Mi
-# Y-Axis Range   = 0km  -> Km   -> 0Mi  -> Mi
+# Info 
+# Velocity Range = 0Kmh -> 24000.0Kmh -> 0Mph -> 15000.0Mph
+# X-Axis Range   = 0km  -> 7822.0Km   -> 0Mi  -> 4860.0Mi
+# Y-Axis Range   = 0km  -> 4500.0Km   -> 0Mi  -> 2796.1Mi 
 
-# Boost Phase = X Moves  
-# Boost Phase  ΔY-Axis   = S0km - EKm     -> S0Mi - EMi   
-# Boost Phase  ΔX-Axis   = S0km - E       -> S0mi - EMi
-# Boost Phase  ΔVelocity = S0Kmh - EKmh   -> S0Mi - EMph#   
+# Boost Phase = 2 Moves  
+# Boost Phase  ΔY-Axis   = S0km - E1200Km     -> S0Mi - E750Mi   
+# Boost Phase  ΔX-Axis   = S0km - E3905       -> S0mi - E2426Mi
+# Boost Phase  ΔVelocity = S0Kmh - E14000Kmh ->  S0Mi  -E14914Mph
+#   
+# Mid Course = 1 Move      
+# Mid Course ΔY-Axis       = S1200Km - E4500Km -> S750Mi   - E2796Mi 
+# Mid Course ΔX-Axis       = S2000Km - E5569km -> S1242Mi  - E2426Mi 
+# Mid Course ΔVelocity     = S14000Kmh - E24000Kmh -> S8700Mph - E14914.24Mph 
+#
+# ReEntry Phase = 2 Moves  
+# ReEntry Phase  ΔY-Axis   = S4500km - E0Km     -> S2796Mi - E500    
+# ReEntry Phase  ΔX-Axis   = S5569km - E7822Km  -> S2426Mi - E14914.24 
+# ReEntry Phase  ΔVelocity = S24000Kmh - 100Kmh -> S14914.24Mph - 62Mph
 
-# MidCourse Phase = X Moves  
-# MidCourse Phase   ΔY-Axis   = Skm  - EKm     -> SMi - EMi   
-# MidCourse Phase   ΔX-Axis   = Skm  - E       -> Smi - EMi
-# MidCourse Phase   ΔVelocity = SKmh - EKmh    -> SMi - EMph
 
-# ReEntry Phase = X Moves  
-# ReEntry Phase  ΔY-Axis   = Skm  - EKm     -> SMi - EMi   
-# ReEntry Phase  ΔX-Axis   = Skm  - E       -> Smi - EMi
-# ReEntry Phase  ΔVelocity = SKmh - EKmh    -> SMi - EMph
+# Start Of Expected OutPut
 
-# End Of Expected OutPut
-# Name = X
-# CurrentMove = X 
-# Y-Axis-Pos = XKm - XMi
-# X-Axis-Pos = XKm - XMi
-# Velocity = XKm - xMi  
-# ETA = X Moves
+# Phase = N/A
+# Name = Nuke[X] 
+# Move = 0 
+# Y-Axis-Pos = 0Km
+# X-Axis-Pos = 0Km
+# Velocity = 0Kmh
+# ETA = 5 Moves
+
+# Phase = Boost Phase 
+# Name = Nuke[X] 
+# Move = 1 
+# Y-Axis-Pos = 600Km
+# X-Axis-Pos = 1952Km
+# Velocity = 7000Kmh
+# ETA = 4 Moves
+
+# Phase = Boost Phase 
+# Name = Nuke[X] 
+# Move = 2
+# Y-Axis-Pos = 1200Km
+# X-Axis-Pos = 3905Km
+# Velocity = 14000Kmh
+# ETA = 3 Moves
+
+# Phase = Mid Course 
+# Name = Nuke[X] 
+# Move = 3
+# Y-Axis-Pos = 4500Km
+# X-Axis-Pos = 4569Km
+# Velocity = 24000Kmh
+# ETA = 2 Moves
+
+# Phase = ReEntry Phase 
+# Name = Nuke[X] 
+# Move = 4
+# Y-Axis-Pos = 2250Km
+# X-Axis-Pos = 5569km
+# Velocity = 12000Kmh
+# ETA = 1 Moves
+
+# Phase = ReEntry Phase 
+# Name = Nuke[X] 
+# Move = 5
+# Y-Axis-Pos = 30Km
+# X-Axis-Pos = 7822Km
+# Velocity = 4000Kmh
+# ETA = 0 Moves
 
 # End Of Expected OutPut
