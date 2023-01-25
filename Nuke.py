c72148b9d7c621d3f799b30c6ef773ac568b59d0
JibrilTurner <you@example.com>
2023-01-24 18:49:48 -0800

    Finish Later

---
+++ Nuke.py
@@ -0,0 +1,71 @@
+# 1/24/23 Made By Jibril 
+# expected outPut
+# https://en.wikipedia.org/wiki/Intercontinental_ballistic_missile#:~:text=Flight%20phases,-The%20following%20flight&text=reentry%2Fterminal%20phase%20(starting%20at,see%20also%20maneuverable%20reentry%20vehicle. 
+
+# General Info 
+# V = Velocity
+# Δ = Change in X 
+# S = Start Of A Phase 
+# E = End Of A Phase
+
+# Info 
+# Velocity Range = 0Kmh -> 24000.0Kmh -> 0Mph -> 15000.0Mph
+# X-Axis Range   = 0km  -> 7822.0Km   -> 0Mi  -> 4860.0Mi
+# Y-Axis Range   = 0km  -> 4500.0Km   -> 0Mi  -> 2796.1Mi 
+
+# Boost Phase = 2 Moves  
+# Boost Phase  ΔY-Axis   = S0km - E1200Km     -> S0Mi - E750Mi   
+# Boost Phase  ΔX-Axis   = S0km - E3905       -> S0mi - E2426Mi
+# Boost Phase  ΔVelocity = S0Kmh - E14000Kmh ->  S0Mi  -E14914Mph
+#   
+# Mid Course = 1 Move      
+# Mid Course ΔY-Axis       = S1200Km - E4500Km -> S750Mi   - E2796Mi 
+# Mid Course ΔX-Axis       = S2000Km - E3905Km -> S1242Mi  - E2426Mi 
+# Mid Course ΔVelocity     = S14000Kmh - E24000Kmh -> S8700Mph - E14914.24Mph 
+#
+# ReEntry Phase = 2 Moves  
+# ReEntry Phase  ΔY-Axis   = S4500km - E0Km     -> S2796Mi - E500    
+# ReEntry Phase  ΔX-Axis   = S3905Km - E24000km -> S2426Mi - E14914.24 
+# ReEntry Phase  ΔVelocity = S24000Kmh - 100Kmh -> S14914.24Mph - 62Mph
+
+
+
+# Total Moves    = 5 Moves 
+
+# Start
+
+# Move = 1 
+# Y-Axis-Pos = 600Km
+# X-Axis-Pos = 1952Km
+# Velocity = 7000
+# ETA = 4  Moves
+
+# Y-Axis-Pos = 1200km
+# X-Axis-Pos = 1952Km
+# Velocity = 3905Kmh
+# ETA = 4 Moves
+# Altitude = 1500.0Km -> 932.9Mi 
+# Speed =  
+
+# Altitude = 0.0  
+# Speed = 0.0 
+# ETA = 5 Moves
+# Altitude = 1500.0Km -> 932.9Mi 
+# Speed =  
+# ETA = 5 Moves
+
+# Altitude = 0.0  
+# Speed = 0.0 
+# ETA = 5 Moves
+# Altitude = 1500.0Km -> 932.9Mi 
+# Speed =  
+# ETA = 5 Moves
+
+# Altitude = 0.0  
+# Speed = 0.0 
+# ETA = 5 Moves
+# Altitude = 1500.0Km -> 932.9Mi 
+# Speed =  
+# ETA = 5 Moves
+
+# End
