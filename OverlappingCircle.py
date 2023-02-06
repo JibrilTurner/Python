af8e0957232710c33d9e09194fd7197bdaf7f598
JibrilTurner <121262046+JibrilTurner@users.noreply.github.com>
2023-02-05 18:11:03 -0800

    Add files via upload

---
+++ OverlappingCircle.py
@@ -0,0 +1,13 @@
+# Python3 code to implement the approach
+
+from math import acos, sqrt
+
+def intersection_area(d,r1,r2): # ans is in km^2
+    d1 = (r1**2 - r2**2 + d**2) / (2 * d)
+    d2 = d - d1
+    area = (r1**2 * acos(d1/r1) - d1 * sqrt(r1**2 - d1**2)) + (r2**2 * acos(d2/r2) - d2 * sqrt(r2**2 - d2**2))
+    return area
+
+
+
+
